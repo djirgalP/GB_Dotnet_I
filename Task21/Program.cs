5e7cb20c73011d39699251d9965622875a759bc9
Djirgal <djirgal.poshtarova@yandex.ru>
2023-02-07 01:16:41 +0300

    Добавлен код по задачам 19, 21, 23

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -7,3 +7,21 @@ using System;
 using static System.Console;
 
 Clear();
+WriteLine("Введите координаты первой точки A ->");
+Write("Введите X1: ");
+int x1 = int.Parse(ReadLine());
+Write("Введите Y1: ");
+int y1 = int.Parse(ReadLine());
+Write("Введите Z1: ");
+int z1 = int.Parse(ReadLine());
+WriteLine("Введите координаты второй точки B ->");
+Write("Введите X2: ");
+int x2 = int.Parse(ReadLine());
+Write("Введите Y2: ");
+int y2 = int.Parse(ReadLine());
+Write("Введите Z2: ");
+int z2 = int.Parse(ReadLine());
+
+double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+
+WriteLine($"Расстояние между точками =  {d:f2}");
