6389c6eb3a26bb742c1118401f63603e4dd202b8
Djirgal <djirgal.poshtarova@yandex.ru>
2023-02-07 19:37:10 +0300

    Решена 24я задача семинара

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+int getSumOfNumbers(int num) 
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++) 
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+Write("Введите число N: ");
+int N = int.Parse(ReadLine());
+WriteLine($"Сумма чисел = {getSumOfNumbers(N)}");
