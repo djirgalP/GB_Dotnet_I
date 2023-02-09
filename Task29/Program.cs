f6f25f51f41b00bd6480c9df7d71de858a5c413d
Djirgal <djirgal.poshtarova@yandex.ru>
2023-02-09 12:29:00 +0300

    Tasks 29 is done

---
+++ Task29/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]*/
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите длину создаваемого массива: ");
+int N = int.Parse(ReadLine());
+WriteLine($"[{String.Join(", ", getRandomArray(N))}]");
+
+int[] getRandomArray(int size) 
+{
+    int[] resultArray = new int[size];
+      
+    for(int i = 0; i < size; i++)
+    {
+        resultArray[i] = new Random().Next(0, 100);
+    }
+    return resultArray; 
+}
+
