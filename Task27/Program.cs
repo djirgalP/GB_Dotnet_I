ec1f4f8c935f4def1b2f5d0bcbccc43c29803b48
Djirgal <djirgal.poshtarova@yandex.ru>
2023-02-09 12:04:34 +0300

    Tasks 25&27 are done

---
+++ Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.*/
+
+using System;
+using static System.Console;
+
+int getSumOfDigits(int num) 
+{
+    int sum = 0;
+    int currentDigit;
+      
+    while (num > 0)
+    {
+        currentDigit = num % 10;
+        sum += currentDigit;
+        num = (num - currentDigit) / 10;    
+    }
+    return sum;
+    
+}
+
+Write("Введите число N: ");
+int N = int.Parse(ReadLine());
+WriteLine($"Сумма цифр в числе = {getSumOfDigits(N)}");
